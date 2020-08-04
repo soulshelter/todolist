87a6025d56d12dc900dd9400ce8df8b96a3f13f9
auroville <auroville.pms@gmail.com>
2020-08-04 17:20:02 +0900

    ADD : completed 구현

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
 import React, { useReducer, useState } from "react";
-import { v4 as uuid } from "uuid";
-
-const initialState = {
-  toDos: [],
-};
-
-const ADD = "add";
-const DEL = "del";
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ADD:
-      return { toDos: [...state.toDos, { text: action.payload, id: uuid() }] };
-    case DEL:
-      return {
-        toDos: state.toDos.filter((toDo) => {
-          return toDo.id !== action.payload;
-        }),
-      };
-    default:
-      return;
-  }
-};
+import reducer, { initialState, ADD, DEL, COMPLETE } from "./reducer";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -47,11 +25,35 @@ function App() {
         {state.toDos.map((toDo) => (
           <li key={toDo.id}>
             <span>{toDo.text}</span>
-            <button onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+            <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
               ❌
-            </button>
+            </span>
+            <span
+              onClick={() => dispatch({ type: COMPLETE, payload: toDo.id })}
+            >
+              ✅
+            </span>
           </li>
         ))}
+      </ul>
+      <ul>
+        {state.completed.length !== 0 && (
+          <>
+            {" "}
+            <h2>Completed</h2>
+            {state.completed.map((toDo) => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                  ❌
+                </span>
+                <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                  ↪️
+                </span>
+              </li>
+            ))}
+          </>
+        )}
       </ul>
     </div>
   );

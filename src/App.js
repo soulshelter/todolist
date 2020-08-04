ced5a0a5f9b8d7c80675c8bddab0776564d717bf
auroville <auroville.pms@gmail.com>
2020-08-04 16:17:20 +0900

    Add : Add to Do 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+
+const initialState = {
+  toDos: [],
+};
+
+const ADD = "increment";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ADD:
+      return { toDos: [...state.toDos, { text: action.payload }] };
+    default:
+      return;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewTodo] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewTodo(value);
+  };
   return (
     <div>
-      <h1>To Dos</h1>
+      <h1>Add to Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="write to do" onChange={onChange} />
+      </form>
+      <ul>
+        <h2>To Docs</h2>
+        {state.toDos.map((todo, index) => (
+          <li key={index}>{todo.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
